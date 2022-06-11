fbb78601b285c3fa916d4c4efb5662c4c4aff027
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-06-11 17:05:20 +0600

    valid js

--- Valid Parentheses/Valid.js
+++ Valid Parentheses/Valid.js
@@ -1,16 +1,20 @@
 // Почти такая же логика как на питоне
 const isValid = (s) => {
+  // Создаю массив куда буду пушить скобки
   const arr = [];
+  // Массив со скобками
   const map = new Map([
     ["(", ")"],
     ["[", "]"],
     ["{", "}"],
   ]);
-
+  // Цикл для перебора
   for (let i = 0; i < s.length; i += 1) {
     // Has по сути тоже самое что и includes
     if (map.has(s[i])) {
+      // Пушу в массив
       arr.push(map.get(s[i]));
+      // Если нет в массиве
     } else if (s[i] !== arr.pop()) {
       return false;
     }
