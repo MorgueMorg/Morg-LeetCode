364feb5ffea787430ee6110c464933306fdc0ce9
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-06-23 22:43:13 +0600

    SymmetricTree

---
+++ SymmetricTree/SymTree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.mirror(root, root)
+    
+    def mirror(self, t1, t2):
+        if not t1 and not t2:
+            return True
+        if not t1 or not t2:
+            return False
+        return t1.val == t2.val and self.mirror(t1.right, t2.left) and self.mirror(t1.left, t2.right)
