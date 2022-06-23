e8619b8f657bed97f22887b54b12a74a4f0a2f75
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-06-23 22:19:52 +0600

    SameTree

--- SameTree/SameTree.js
+++ SameTree/SameTree.js
@@ -11,12 +11,19 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
+
+ // ! Использовал рекурсию, проверил каждое поддерево(глубокий поиск)
+ // ! Параметры val, left и right прописаны сверху в конструкторе this, обозначают стороны дерева
+
 var isSameTree = function (p, q) {
   if (!p && !q) {
+    // если оба дерева пусты(false), то значит одинаковы и возвращаю true
     return true;
   } else if (!p || !q || p.val != q.val) {
+    // Если их значение не равны друг другу или одно пустое, возвращаю false
     return false;
   } else {
+    // Либо же возвращаю рекурсию со сторонами деревьев
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
   }
 };
--- SameTree/SameTree.py
+++ SameTree/SameTree.py
@@ -4,8 +4,14 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# ! Использовал рекурсию, проверил каждое поддерево(глубокий поиск)
+# ! Параметры val, left и right прописаны сверху в конструкторе init, обозначают стороны дерева
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # if если оба дерева пусты(false), то значит одинаковы и возвращаю true
         if not p and not q: return True
+        # Если их значение не равны друг другу или одно пустое, возвращаю false
         if not p or not q or p.val != q.val: return False
+        # Либо же возвращаю рекурсию со сторонами деревьев, не забываю self ввиду ооп
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
