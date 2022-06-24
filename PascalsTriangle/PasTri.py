1b3950b1cdeaebb1c3ea380e1aa43a95ef0ed47a
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-06-24 19:56:07 +0600

    PascalsTriangle

---
+++ PascalsTriangle/PasTri.py
@@ -0,0 +1,10 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+        for i in range(numRows - 1):
+            temp = [0] + res[-1] + [0]
+            row = []
+            for j in range(len(res[-1]) + 1):
+                row.append(temp[j] + temp[j + 1])
+            res.append(row)
+        return res
