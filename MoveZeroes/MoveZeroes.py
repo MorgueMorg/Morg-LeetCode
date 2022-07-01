e6d74d5e78a387462ceaa16b3a9fe931eb964eb6
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-07-01 20:30:34 +0600

    MoveZeroes

---
+++ MoveZeroes/MoveZeroes.py
@@ -0,0 +1,25 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = len(nums)
+        i = 0
+        while i < l:
+            if nums[i] == 0:
+                nums.append(nums.pop(i))
+                l -= 1
+            else:
+                i += 1
+        return nums
+
+    
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in nums:
+            if i == 0:
+                nums.remove(i)
+                nums.append(i)
