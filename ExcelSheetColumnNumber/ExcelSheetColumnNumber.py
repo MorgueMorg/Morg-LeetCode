eb84a7831046148a99a410d7d2deb31f2e9782e1
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-07-20 11:52:00 +0600

    ExcelSheetColumnNumber

---
+++ ExcelSheetColumnNumber/ExcelSheetColumnNumber.py
@@ -0,0 +1,6 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        res = 0
+        for i, c in enumerate(reversed(columnTitle.upper())):
+            res += (26 ** i) * (ord(c) - ord("A") + 1)
+        return res  
