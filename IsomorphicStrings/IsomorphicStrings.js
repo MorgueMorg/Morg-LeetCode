e85c694c96ef5ec759880d5c99d8829e5e3c1397
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-07-21 03:43:42 +0600

    IsomorphicStrings

---
+++ IsomorphicStrings/IsomorphicStrings.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  let hasht = {};
+  for (let i = 0; i < s.length; i++) {
+    if (!hasht["s" + s[i]]) hasht["s" + s[i]] = t[i];
+    if (!hasht["t" + t[i]]) hasht["t" + t[i]] = s[i];
+    if (t[i] != hasht["s" + s[i]] || s[i] != hasht["t" + t[i]]) return false;
+  }
+  return true;
+};
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  let arr1 = {};
+  let arr2 = {};
+  for (let i = 0; i < s.length; i++) {
+    if (arr1[s[i]] != arr2[t[i]]) {
+      return false;
+    } else {
+      arr1[s[i]] = i;
+      arr2[t[i]] = i;
+    }
+  }
+  return true;
+};
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  let map = new Map();
+  if (new Set(s).size != new Set(t).size) return false;
+  for (let i = 0; i < s.length; i++) {
+    if (map.get(s[i]) && map.get(s[i]) != t[i]) {
+      return false;
+    } else {
+      map.set(s[i], t[i]);
+    }
+  }
+  return true;
+};
