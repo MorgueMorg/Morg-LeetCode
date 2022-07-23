4d04332371b5b6c04b7cc3c2f4a73c294f51d376
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-07-23 10:53:08 +0600

    WordPattern

---
+++ WordPattern/WordPattern.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function (pattern, s) {
+  const words = s.split(" ");
+  const map = new Map();
+
+  if (words.length !== pattern.length) return false;
+  if (new Set(words).size !== new Set(pattern).size) return false;
+
+  for (let i = 0; i < pattern.length; i++) {
+    if (map.has(pattern[i]) && map.get(pattern[i]) !== words[i]) return false;
+    map.set(pattern[i], words[i]);
+  }
+  return true;
+};
