a31b93cfd2a0131a4f4061cde38eb8ff63ae7079
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-06 11:31:42 +0600

    ValidPalindromeII

---
+++ ValidPalindromeII/ValidPalindromeII.py
@@ -0,0 +1,10 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                one, two = s[left:right], s[left + 1:right + 1]
+                return one == one[::-1] or two == two[::-1]
+            left += 1
+            right -= 1
+        return True
