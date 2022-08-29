07649f633212ee27b7c2b45d1cb531847d7b3ef4
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-08-29 17:32:28 +0600

    NumberOfIslands

---
+++ NumberOfIslands/NumberOfIslands.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid: return 0
+        def dfs(grid, i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != "1": return 
+            grid[i][j] = '#'
+            dfs(grid, i+1, j)
+            dfs(grid, i-1, j)
+            dfs(grid, i, j+1)
+            dfs(grid, i, j-1)
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    dfs(grid, i, j)
+                    count += 1
+        return count
