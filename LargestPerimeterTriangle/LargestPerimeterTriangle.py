b1a5c13e37edca2e295446ddcb93512d75ca412d
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-09-07 11:28:32 +0600

    LargestPerimeterTriangle

---
+++ LargestPerimeterTriangle/LargestPerimeterTriangle.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums = sorted(nums, reverse = True)
+        for i in range(len(nums) - 2):
+            if nums[i] < nums[i + 1] + nums[i + 2]:
+                return nums[i] + nums[i + 1] + nums[i + 2]
+        return 0
