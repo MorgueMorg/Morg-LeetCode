bf6ee3275228e4144e21114d363ec614c9a5463f
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-09-17 23:50:52 +0600

    CheckIfNAndItsDoubleExist

--- CheckIfNAndItsDoubleExist/CheckIfNAndItsDoubleExist.py
+++ CheckIfNAndItsDoubleExist/CheckIfNAndItsDoubleExist.py
@@ -7,3 +7,13 @@ class Solution:
             else:
                 dct[i] = 1
         return False
+
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for i in arr:
+            if i * 2 in arr and i != 0:
+                return True
+            elif i == 0 and (arr.count(i) > 1):
+                return True
+        return False
