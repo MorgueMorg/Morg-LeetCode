c325349436f1903d24684bb428d0df2435c2643e
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-10-08 20:18:15 +0600

    ReverseLinkedList

---
+++ ReverseLinkedList/ReverseLinkedList.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  if (!head || !head.next) {
+    return head;
+  }
+  let newHead = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+  return newHead;
+};
