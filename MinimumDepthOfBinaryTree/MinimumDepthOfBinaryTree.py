eac3e9f815970582749dd173f2caaa6b962d1915
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-10-09 20:15:37 +0600

    MinimumDepthOfBinaryTree

---
+++ MinimumDepthOfBinaryTree/MinimumDepthOfBinaryTree.py
@@ -0,0 +1,34 @@
+# ! Recursion
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        elif root.right and root.left:
+            return min (self.minDepth(root.right), self.minDepth(root.left)) + 1
+        elif root.right:
+            return self.minDepth(root.right) + 1
+        else:
+            return self.minDepth(root.left) + 1
+
+
+# ! DFS
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        if None in [root.left, root.right]:
+            return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        else:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
