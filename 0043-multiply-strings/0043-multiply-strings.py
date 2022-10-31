92b6789a994827b079d5ec1a75faafcfa61ecc5d
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-10-31 19:03:10 +0600

    Time: 67 ms (63.66%), Space: 13.7 MB (98.15%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,4 +1,24 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        sum_ = int(num1) * int(num2)
-        return str(sum_)
+        dic = {}
+        for i in range(10):
+            dic[str(i)] = i
+        
+        a = 0
+        deci_pos = 1 
+        for i in range(len(num1)-1,-1,-1):
+            a += dic[num1[i]]*deci_pos
+            deci_pos *= 10
+            
+        b = 0
+        deci_pos = 1
+        for i in range(len(num2)-1,-1,-1):
+            b += dic[num2[i]]*deci_pos
+            deci_pos *= 10
+		
+        return str(a*b)
+        
+        
+        
+        # sum_ = int(num1) * int(num2)
+        # return str(sum_)
