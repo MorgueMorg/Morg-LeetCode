0ec824cdb4b4e423363d44df82ad8effd1713d22
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-11-11 08:59:54 +0600

    AddTwoIntegers

---
+++ AddTwoIntegers/AddTwoIntegers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        arr = []
+        arr.append(num1)
+        arr.append(num2)
+        return sum(arr)
+
+        # return num1 + num2
