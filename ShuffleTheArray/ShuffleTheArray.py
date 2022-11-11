bff7900445c3ef8378c48778f01923252e5f80f3
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-11-11 09:28:27 +0600

    ShuffleTheArray

---
+++ ShuffleTheArray/ShuffleTheArray.py
@@ -0,0 +1,7 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        arr = []
+        for i in range(n):
+            arr.append(nums[i])
+            arr.append(nums[i + n])
+        return arr
