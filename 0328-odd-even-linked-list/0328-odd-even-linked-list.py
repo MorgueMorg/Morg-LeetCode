f927beb2a4f54ee8e281a9e2a6922447856eeae8
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2022-12-06 13:36:15 +0600

    Time: 52 ms (83.38%), Space: 16.5 MB (78.70%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -5,17 +5,19 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None: return head
-        odd = head
-        even = head.next
+        if not head: return head # Проверка на пустой список
+        odd = head # Первый узел
+        even = head.next # Если в листе один узел, то это будет None
         eHead = even # Переменная где начинаются четные узлы
         
-        while even and even.next:
+        while even and even.next: # Цикл поначалу бесполезен, если только один узел
             odd.next = odd.next.next
             even.next = even.next.next
-            
+            # После двух операций выше, чёт/нечёт остаются на тех же местах
+            # Поэтому двумя операциями ниже, меняем их на след узел
             odd = odd.next
             even = even.next
-        
+        # Нечетный указатель сейчас указывает на последний узел списка нечетных узлов
         odd.next = eHead
+        # Возвращаю нечет в начале самого списка
         return head
