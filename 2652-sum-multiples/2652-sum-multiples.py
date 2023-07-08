927ef4e7b7ac120c1a5a27ab128eada88f42bba4
Dzhanaliev Aktan <dzhanaliev.aktan@gmail.com>
2023-07-08 22:31:28 +0600

    Time: 96 ms (72.40%), Space: 16.2 MB (63.84%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        counter = 0
+        for i in range(3, n + 1):
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+                counter += i
+        return counter
